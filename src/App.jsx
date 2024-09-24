2fcaa8c0d6223829f91d0bf035f8afeb6a76d2da
Anjali <anjali@Anjalis-MacBook-Air.local>
2024-09-24 15:35:15 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { TodoProvider } from './contexts/TodoContext'
-import { json } from 'react-router-dom'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
+
+
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -31,15 +33,32 @@ function App() {
     }
   }, [])
 
-
   useEffect(() => {
-    JSON.parse(localStorage.setItem("todos", JSON.stringify(todos)))
+    (localStorage.setItem("todos", JSON.stringify(todos)))
   }, [todos])
-  return (
 
-    <TodoProvider value={{ todos, addTodo, deleteTodo, updateTodo, toggleCompleted }}>
-      <div>
-        <h1>Todo List</h1>
+
+
+
+  return (
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleCompleted }}>
+      <div className="bg-[#021526] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Write down you daily routine</h1>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id}
+                className='w-full'
+              >
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </TodoProvider>
   )
