487cdd239c08a2ad8ce447871f0a7f16f5a4f28f
Anjali <anjali@Anjalis-MacBook-Air.local>
2024-09-24 19:23:34 +0530

    completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ function Footer({ completedTask, totalTask }) {
     return (
         <>
             {/* footer to track completed and total task */}
-            <div className='bg-[#6EACDA] w-full p-4 mt-4'>
+            <div className='bg-[#6EACDA] text-center p-4 mt-4'>
                 <span className='m-4'>Completed Task : {completedTask}</span>
-                <span>Total Task : {totalTask}</span>
+                <span className='m-4'>Total Task : {totalTask}</span>
             </div>
         </>
     )
